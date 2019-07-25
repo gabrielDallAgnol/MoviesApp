fba02a1d37865b41acab5625e2375e7830f2dec0
Gabriel Schmit Dall'agnol <gabrielsdagnol@gmail.com>
2019-07-25 00:03:00 -0300

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Palmsoft  on 25/07/19.
+//  Copyright © 2019 Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
