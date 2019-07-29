3bcc0896da00a526690dba78f3c2f9848039dab4
gabrielDallAgnol <45344734+gabrielDallAgnol@users.noreply.github.com>
2019-07-29 16:29:27 -0300

    Dismiss Keyboard

--- MoviesApp/Features/Movies/MoviesList/MoviesListController.swift
+++ MoviesApp/Features/Movies/MoviesList/MoviesListController.swift
@@ -26,6 +26,12 @@ class MoviesListController: UIViewController {
         collectionView.reloadData()
         tableView.reloadData()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
+    
 }
 
 // MARK: - TableView DataSource and Delegate
